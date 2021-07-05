275da5c861e1d33c2829240dd5ea17262a9822d7
Sham-An <shamraev@inbox.ru>
2021-07-05 20:57:06 +0300

    Number 4

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('PyCharm3')
-    print_hi('PyCharm5 ')
+    print_hi('PyCharm4')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
