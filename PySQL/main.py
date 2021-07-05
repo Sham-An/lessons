ffb92298803dc532b884195dcc853fd34ca4c2da
Sham-An <shamraev@inbox.ru>
2021-07-05 20:42:36 +0300

    Number 3

--- PySQL/main.py
+++ PySQL/main.py
@@ -11,7 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('PyCharm1')
     print_hi('PyCharm3')
 
 
